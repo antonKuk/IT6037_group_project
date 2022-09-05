2bfc07468700b4f4f28fe0379e99eb6de7090715
ztur211 <ztur211@mywhitecliffe.com>
2022-09-06 02:50:33 +1200

    added login modal

--- discovery_app/articles/urls.py
+++ discovery_app/articles/urls.py
@@ -11,7 +11,4 @@ urlpatterns = [
     path("article/<slug:slug>",views.DetailArticlelView.as_view(),name="detail-page"),
     path("article/<slug:slug>/update/",views.UpdateArticleView.as_view(),name="update-page"),
     path("article/<slug:slug>/delete/",views.DeleteArticleView.as_view(),name="delete-page"),
-
-
-
-] 
+]
