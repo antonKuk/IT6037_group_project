627cf1bbbfcb1afeca003ecace07b8aed582a603
@conrado0635 <ccab211@mywhitecliffe.com>
2022-09-08 03:07:20 +1200

    update user testcase

--- articles/models.py
+++ articles/models.py
@@ -7,27 +7,27 @@ from django.urls import reverse
 
 '''Article Model'''
 
-# class Articles(models.Model):
-#     slug = models.SlugField(unique=True, db_index=True,null=True)
-#     article_name = models.CharField(max_length=150)
-#     about = models.TextField(validators=[MinLengthValidator(10)])
+class Articles(models.Model):
+    slug = models.SlugField(unique=True, db_index=True,null=True)
+    article_name = models.CharField(max_length=150)
+    about = models.TextField(validators=[MinLengthValidator(10)])
 
-#     date_born = models.DateField()
-#     date_died = models.DateField()
-#     nationality = models.CharField(max_length=150)
-#     known_for = models.CharField(max_length=255)
-#     notable_work =models.CharField(max_length=255)
+    date_born = models.DateField()
+    date_died = models.DateField()
+    nationality = models.CharField(max_length=150)
+    known_for = models.CharField(max_length=255)
+    notable_work =models.CharField(max_length=255)
 
-#     def __str__(self):
-#         return self.article_name
+    def __str__(self):
+        return self.article_name
 
-#     def get_absolute_url(self):
-#         return reverse("home-page")
+    def get_absolute_url(self):
+        return reverse("home-page")
         
 
-#     def save(self, *args, **kwargs):
-#             self.slug = slugify(self.article_name)
-#             super(Articles,self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+            self.slug = slugify(self.article_name)
+            super(Articles,self).save(*args, **kwargs)
 
 
 class Category(models.Model):
--- users/tests.py
+++ users/tests.py
@@ -1,21 +1,19 @@
-# from datetime import date
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
-# from django.urls import reverse
+from datetime import date
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# from blog.models import Post,Comment,Author
+from articles.models import Article,Category,Type
 
-# '''Model Test'''
+'''Model Test'''
 
-# class PostTests(TestCase):
-#     @classmethod
-#     def setUpTestData(self):
-#         self.author = Author.objects.create(
-#             first_name="testuser", last_name="lastname", email_address="test@gmail.com")
-
-#         self.post = Post.objects.create(title="this is a test!",excerpt="test!",date = date.today(),content="content test")
+class ArticleTests(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.article = Article.objects.create(
+            article_name="test", about="about", natinality="Filipino")
 
 #     def test_model_content(self):
 #         self.assertEqual(self.post.title, "this is a test!")
@@ -23,27 +21,27 @@
 #         self.assertEqual(self.post.date, date.today())
 #         self.assertEqual(self.post.content, "content test")
 
-# '''User Test'''
+'''User Test'''
 
-# class UserAccountTests(TestCase):
+class UserAccountTests(TestCase):
 
-#     def test_new_superuser(self):
-#         db = get_user_model()
-#         super_user = db.objects.create_superuser(email='new_admin@gmail.com', username='username', first_name='firstname')
-#         self.assertEqual(super_user.email, 'new_admin@gmail.com')
-#         self.assertEqual(super_user.username, 'username')
-#         self.assertEqual(super_user.first_name, 'firstname')
-#         self.assertTrue(super_user.is_superuser)
-#         self.assertTrue(super_user.is_staff)
-#         self.assertTrue(super_user.is_active)
-#         self.assertEqual(str(super_user), "username")
+    def test_new_superuser(self):
+        db = get_user_model()
+        super_user = db.objects.create_superuser(email='new_admin@gmail.com', username='username', first_name='firstname')
+        self.assertEqual(super_user.email, 'new_admin@gmail.com')
+        self.assertEqual(super_user.username, 'username')
+        self.assertEqual(super_user.first_name, 'firstname')
+        self.assertTrue(super_user.is_superuser)
+        self.assertTrue(super_user.is_staff)
+        self.assertTrue(super_user.is_active)
+        self.assertEqual(str(super_user), "username")
 
-#     def test_new_user(self):
-#         db = get_user_model()
-#         user = db.objects.create_user(username='username',email='samuel@gmail.com',first_name = 'firstname')
-#         self.assertEqual(user.email, 'samuel@gmail.com')
-#         self.assertEqual(user.username, 'username')
-#         self.assertEqual(user.first_name, 'firstname')
-#         self.assertFalse(user.is_superuser)
-#         self.assertFalse(user.is_staff)
+    def test_new_user(self):
+        db = get_user_model()
+        user = db.objects.create_user(username='username',email='samuel@gmail.com',first_name = 'firstname')
+        self.assertEqual(user.email, 'samuel@gmail.com')
+        self.assertEqual(user.username, 'username')
+        self.assertEqual(user.first_name, 'firstname')
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
   
