a7ed089de21c700e984e93e0de8f8756f3116cd2
antonKuk <akuk211@mywhitecliffe.com>
2022-09-08 21:35:09 +1200

    in progress

--- discovery_app/articles/models.py
+++ discovery_app/articles/models.py
@@ -1,7 +1,7 @@
 from django.utils.text import slugify
 from pickle import TRUE
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.urls import reverse
 
 '''Article Model'''
@@ -53,8 +53,8 @@ class Article(models.Model):
 #     image = models.ImageField(upload_to="posts", null=True)
     # date_born = models.DateField(blank=True)
     # date_died = models.DateField(blank=True)
-    date_born = models.PositiveSmallIntegerField(null=True, validators=[MinLengthValidator(2022)])
-    date_died = models.PositiveSmallIntegerField(null=True, validators=[MinLengthValidator(2022)])
+    date_born = models.PositiveSmallIntegerField(null=True, blank=True, validators=[MaxValueValidator(2022)])
+    date_died = models.PositiveSmallIntegerField(null=True, blank=True, validators=[MaxValueValidator(2022)])
 
     nationality = models.CharField(max_length=150, blank=True)
     known_for = models.CharField(max_length=255, blank=True)
@@ -62,7 +62,7 @@ class Article(models.Model):
     location = models.CharField(max_length=100, blank=True)
     medium = models.CharField(max_length=150, blank=True)
     # year = models.DateField(null=True)
-    year = models.PositiveSmallIntegerField(null=True)
+    year = models.PositiveSmallIntegerField(null=True, blank=True, validators=[MaxValueValidator(2022)])
 
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, related_name="category")
--- discovery_app/common_commands.md
+++ discovery_app/common_commands.md
@@ -12,3 +12,9 @@ pip install django-crispy-forms     #for bootstrap forms
 pip install crispy-bootstrap5
 
 https://simpleisbetterthancomplex.com/tutorial/2016/07/26/how-to-reset-migrations.html How to Reset Migrations
+
+git checkout -r
+git checkout <commit_code>
+git log --oneline --all --graph --decorate  $(git reflog | awk '{print $1}') #this console command will show you all commits
+#And if you want to make commits while you're there, go ahead and make a new branch while you're at it:
+git checkout -b <new_branch_name> <commit_code> 
